376b94de179b3662c026fbe979a4df1f3fed35c3
ODrimm <ystos.gaming@gmail.com>
2023-05-23 11:59:00 +0200

    animations setup

--- script.js
+++ script.js
@@ -9,15 +9,20 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.set(-1.882, 1.543, -3.822);
 camera.rotation.set(THREE.Math.degToRad(0), THREE.Math.degToRad(180), THREE.Math.degToRad(0));
 
+const clock = new THREE.Clock();
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.physicallyCorrectLights = true;
+renderer.physicallyCorrectLights = false;
 renderer.toneMapping = THREE.ReinhardToneMapping;
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 
 renderer.antialias = true;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -26,15 +31,64 @@ controls.update();
 
 const loader = new GLTFLoader();
 
+let plateBounce;
+let forkBounce;
+let plate2Bounce;
+let steakBounce;
+let steakBounceSK;
+
+let plateBounceAction
+let forkBounceAction
+let plate2BounceAction
+let steakBounceAction
+let steakBounceSKAction
+
+let mixer;
+
 loader.load('assets/3d/objects.gltf', function (gltf) {
 
-    gltf.parser.getDependencies('material').then((materials) => {
+    for (let i = 0; i < gltf.animations.length; i++) {
+        if (gltf.animations[i].name == "Plate_bounce.002") {
 
-        console.log(materials);
+            plateBounce = gltf.animations[i];
 
-    });
+        } else if (gltf.animations[i].name == "Fork_Bounce.002") {
+
+            forkBounce = gltf.animations[i];
+
+        } else if (gltf.animations[i].name == "Plate2_bounce.002") {
+
+            plate2Bounce = gltf.animations[i];
+
+        } else if (gltf.animations[i].name == "Steak_Bounce.002") {
+
+            steakBounce = gltf.animations[i];
+
+        } else if (gltf.animations[i].name == "SteakBounce_SK") {
+
+            steakBounceSK = gltf.animations[i];
+        }
+    }
+
+    mixer = new THREE.AnimationMixer(gltf.scene);
+
+    plateBounceAction = mixer.clipAction(plateBounce);
+    plateBounceAction.play();
+
+    forkBounceAction = mixer.clipAction(forkBounce);
+    forkBounceAction.play();
+
+    plate2BounceAction = mixer.clipAction(plate2Bounce);
+    plate2BounceAction.play();
+
+    steakBounceAction = mixer.clipAction(steakBounce);
+    steakBounceAction.play();
+
+    steakBounceSKAction = mixer.clipAction(steakBounceSK);
+    steakBounceSKAction.play();
 
     gltf.scene.traverse(function (child) {
+
 
         child.castShadow = true;
         child.receiveShadow = true;
@@ -42,6 +96,7 @@ loader.load('assets/3d/objects.gltf', function (gltf) {
         if (child.name == "Steak") {
             let map = child.material.map;
             child.material = new THREE.MeshToonMaterial({ color: 0xffffff, map: map });
+            child.material.side = THREE.FrontSide;
         }
         if (child.name == "Retopo_Fork001") {
             let map = child.material.map;
@@ -75,10 +130,10 @@ loader.load('assets/3d/objects.gltf', function (gltf) {
 
 const light = new THREE.SpotLight(0xfbffe0);
 light.position.set(3.220, 4.980, -2.546);
-light.distance = 8.25;
-light.intensity = 400;
+light.distance = 10;
+light.intensity = 45;
 light.angle = 0.914;
-light.decay = 1.72;
+light.decay = 2;
 light.castShadow = true;
 
 light.shadow.camera.left = -5
@@ -91,11 +146,40 @@ light.shadow.mapSize.height = 2048
 scene.add(light);
 
 const ambientLight = new THREE.AmbientLight(0x222222);
-ambientLight.intensity = 5.3;
+ambientLight.intensity = 2.5;
 scene.add(ambientLight);
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 5);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube1 = new THREE.Mesh(geometry, material);
+cube1.position.set(1.2, 3.568, -0.724);
+cube1.rotation.set(THREE.Math.degToRad(0), THREE.Math.degToRad(38.29), THREE.Math.degToRad(0));
+cube1.castShadow = true;
+const cube2 = new THREE.Mesh(geometry, material);
+cube2.position.set(1.481, 3.167, -2.233);
+cube2.rotation.set(THREE.Math.degToRad(-180), THREE.Math.degToRad(53.41), THREE.Math.degToRad(-180));
+cube2.castShadow = true;
+const cube3 = new THREE.Mesh(geometry, material);
+cube3.position.set(3.247, 3.587, -2.605);
+cube3.rotation.set(THREE.Math.degToRad(0), THREE.Math.degToRad(38.46), THREE.Math.degToRad(0));
+cube3.castShadow = true;
+const cube4 = new THREE.Mesh(geometry, material);
+cube4.position.set(2.598, 3.691, -0.802);
+cube4.rotation.set(THREE.Math.degToRad(0), THREE.Math.degToRad(132.96), THREE.Math.degToRad(0));
+cube4.castShadow = true;
+
+scene.add(cube1, cube2, cube3, cube4);
+
+
+
 
 function animate() {
     requestAnimationFrame(animate);
+
+    if (mixer) {
+        mixer.update(clock.getDelta())
+    }
 
     controls.update();
 
